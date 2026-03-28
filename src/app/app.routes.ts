91a42baaa425e7b28b961c8d8a945de1ed4698a1
hamzafoy <hamzaoflouisville@gmail.com>
2026-03-28 02:07:27 -0400

    updates to muhsineen site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { CalendarComponent } from './calendar/calendar.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingPageComponent, pathMatch: 'full' },
+  { path: '', component: LandingPageComponent, title: 'Home', pathMatch: 'full' },
   { path: 'about', component: AboutUsComponent, title: 'About Us' },
   { path: 'calendar', component: CalendarComponent, title: 'Community Calendar' },
   // Add more routes here as needed
