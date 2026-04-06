287eeee74a0498097b5e000690ab3254a16758df
hamzafoy <hamzaoflouisville@gmail.com>
2026-04-06 11:05:49 -0400

    added member roster to directory page

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -6,11 +6,13 @@ import { LayoutService } from '../services/layout.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-directory',
   standalone: true,
-  imports: [CommonModule, NavigationComponent, NgbCarousel, NgbSlide, MatProgressSpinner],
+  imports: [CommonModule, NavigationComponent, NgbCarousel, NgbSlide, MatProgressSpinner, MatIconModule, MatExpansionModule],
   templateUrl: './directory.component.html',
   styleUrls: ['./directory.component.css']
 })
@@ -44,6 +46,64 @@ export class DirectoryComponent implements OnInit {
   }
   public layout: string = '';
   public isLoading: boolean = true;
+
+  public members: string[] = [
+    "Abaiz Choudhri",
+    "Abdirahman Burqe",
+    "Abdifatah Omar",
+    "Abdulaziz Ahmad",
+    "Abdurahman Bamedhan",
+    "Abdurahman Mahmoud",
+    "Adam Zahaf",
+    "Ahmad Arar",
+    "Ahmad Haji",
+    "Ahmad Khalifa",
+    "Ahmad Qazzaz",
+    "Al-Ameen Salaudeen",
+    "Amine Hamlouchi",
+    "Ammar Fatihullah",
+    "Anwar Adam",
+    "Areeb Lilamwala",
+    "Arshak Naseer",
+    "Asha Peoples",
+    "Badruddin Aden",
+    "Bakar Haythar",
+    "Brandon Nguyen",
+    "Faysal Bulbul",
+    "Greiler Abrahantes",
+    "Hamza Foy",
+    "Hanable Sadiq",
+    "Imraan Siddiqui",
+    "Jonathan Macks",
+    "Kaab Awaan",
+    "Kendrick Jones",
+    "Khalid Zweiqat",
+    "Lamine Kamara",
+    "Logan Schroeder",
+    "Mahad Muhammad",
+    "Mahmud Estes",
+    "Matthew Bahira",
+    "Mohamed Ali",
+    "Mohamed Sharieff",
+    "Mouctar Barrie",
+    "Muhammad Sillah",
+    "Musa Haythar",
+    "Nashaz Naseem",
+    "Omar Jammeh",
+    "Omar Khalifa",
+    "Ramadan Mohamed",
+    "Rommie Kader",
+    "Samir Patel",
+    "Talha Faiz",
+    "Tawfeeq Fields",
+    "Umar Seyal",
+    "Walid Elmajbri",
+    "Ward Abdellah",
+    "Yousef Ismail",
+    "Zane Ghalib"
+  ];
+
+  nameRows: string[][] = [];
   
   public images: string[] = [
     '../assets/directory_imgs/directory_hamza_foy.png',
@@ -58,6 +118,10 @@ export class DirectoryComponent implements OnInit {
   ]
 
   ngOnInit(): void {
+    const chunkSize = (this.IsLaptopViewport || this.IsLargeViewport || this.IsXLargeViewport) ? 6 : 3;
+    for (let i = 0; i < this.members.length; i += chunkSize) {
+      this.nameRows.push(this.members.slice(i, i + chunkSize));
+    }
     if (this.IsMobileViewport || this.IsTabletViewport) {
       this.layout = 'mobile';
     } else {
